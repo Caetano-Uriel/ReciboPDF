809db90798858320bf6ea7d4bec4e7c120caf46e
Caetano-Uriel <urielferrazc@gmail.com>
2025-12-30 17:44:34 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     const hoje = new Date();
     
-    // Datas
+    // 1. Data Numérica
     const dia = String(hoje.getDate()).padStart(2, '0');
     const mes = String(hoje.getMonth() + 1).padStart(2, '0');
     const ano = hoje.getFullYear();
     document.getElementById('dataAtual').value = `DD/MM/AAAA`;
 
-    const meses = [
-        "janeiro", "fevereiro", "março", "abril", "maio", "junho", 
-        "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"
-    ];
-    const nomeMes = meses[hoje.getMonth()];
-    const textoRodape = `Almenara - MG, ${hoje.getDate()} de ${nomeMes} de ${ano}`;
-    document.getElementById('inputCidadeData').value = textoRodape;
-
-    // Início
+    // 2. Inicializa
     adicionarLinha();
     document.getElementById('btnAdd').addEventListener('click', adicionarLinha);
 });
@@ -24,7 +16,7 @@ function adicionarLinha() {
     const tbody = document.getElementById('tabelaItens').getElementsByTagName('tbody')[0];
     const row = tbody.insertRow();
 
-    // 1. QTD (Input)
+    // 1. QTD
     const cellQtd = row.insertCell(0);
     cellQtd.className = 'col-qtd';
     const inputQtd = document.createElement('input');
@@ -32,18 +24,17 @@ function adicionarLinha() {
     inputQtd.addEventListener('input', calcularTotal);
     cellQtd.appendChild(inputQtd);
 
-    // 2. DESCRIÇÃO (DIV EDITÁVEL - MUDANÇA IMPORTANTE)
+    // 2. DESCRIÇÃO (DIV EDITÁVEL)
     const cellDesc = row.insertCell(1);
     cellDesc.className = 'col-desc';
     
-    // Criamos uma DIV em vez de Input para permitir quebra de linha infinita
     const divDesc = document.createElement('div');
     divDesc.className = 'celula-editavel';
     divDesc.contentEditable = "true";
     
     cellDesc.appendChild(divDesc);
 
-    // 3. VALOR (Input com R$)
+    // 3. VALOR (Com R$)
     const cellValor = row.insertCell(2);
     cellValor.className = 'col-unit';
     const wrapperValor = document.createElement('div');
@@ -57,7 +48,7 @@ function adicionarLinha() {
     wrapperValor.appendChild(inputValor);
     cellValor.appendChild(wrapperValor);
 
-    // 4. TOTAL (Input com R$)
+    // 4. TOTAL (Com R$)
     const cellTotal = row.insertCell(3);
     cellTotal.className = 'col-total';
     const wrapperTotal = document.createElement('div');
